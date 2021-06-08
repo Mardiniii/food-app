88a7844ef1e5a521196b47b4765f4128cda1d3f1
mardiniii <sezama03@hotmail.com>
2021-06-08 16:40:21 -0400

    Use `useEffect` hook to animate cart modal button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import React from "react";
 
@@ -8,14 +8,35 @@ import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHightlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx; // object destructuring
 
-  const numberOfCartItems = cartCtx.items.reduce((result, item) => {
+  const numberOfCartItems = items.reduce((result, item) => {
     return result + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHightlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
